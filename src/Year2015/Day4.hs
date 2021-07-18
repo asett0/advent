1cc232ffb4c9a3cd875d5d89349a875086563689
asett0 <amitsett0@gmail.com>
2021-07-18 20:16:30 +0800

    added bystring package and completed Day4

--- src/Year2015/Day4.hs
+++ src/Year2015/Day4.hs
@@ -1,3 +1,34 @@
 module Year2015.Day4 where
 
-import Crypto.Hash
+import Crypto.Hash (MD5 (MD5), hashWith)
+import Data.ByteString.Char8 (pack)
+import Data.Foldable (find)
+import Util (getData)
+
+input :: IO String
+input = getData "data/2015/Day4.txt"
+
+md5Hash :: String -> String
+md5Hash s = show $ hashWith MD5 $ pack s
+
+leadingZeros :: String -> Int
+leadingZeros "" = 0
+leadingZeros (c : cs) = if c == '0' then 1 + leadingZeros cs else 0
+
+enoughZeros :: String -> Int -> Bool
+enoughZeros s n = leadingZeros s >= n
+
+findHash :: String -> Int -> Maybe Int
+findHash secretKey n = find (\m -> enoughZeros (md5Hash $ secretKey ++ show m) n) [0 ..]
+
+y2015d4ex1 :: IO ()
+y2015d4ex1 = do
+  contents <- input
+  let ans = maybe "Never finds Hash" show $ findHash contents 5
+  putStrLn ans
+
+y2015d4ex2 :: IO ()
+y2015d4ex2 = do
+  contents <- input
+  let ans = maybe "Never finds Hash" show $ findHash contents 6
+  putStrLn ans
