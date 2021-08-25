e65db32cc9c9838de523d8ddd717ebec0e566cb9
asett0 <amitsett0@gmail.com>
2021-08-25 17:29:05 +0800

    added tests for Day 1

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,10 @@
+import Test.Hspec (describe, hspec, it)
+import Test.QuickCheck (Testable (property))
+import Year2015.Day1 (moveFloor)
+
+prop_moveFloorInverse :: Int -> Bool
+prop_moveFloorInverse floor = (moveFloor (moveFloor floor ')') '(' == floor) && (moveFloor (moveFloor floor '(') ')' == floor)
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+  describe "Day 1 tests" $ do it "returns the original floor if ')' and '(' are called in succession or vice versa" $ property prop_moveFloorInverse
